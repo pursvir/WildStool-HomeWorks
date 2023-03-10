a1177a5b15034c16639a2dce0aa37cb01fcafa06
unknown <112648089+pursvir@users.noreply.github.com>
2023-03-10 20:44:57 +0300

    НЫЫЫЫААААААААА НАХУЙ

--- DRY-KISS-YAGNI-SOLID/Program.cs
+++ DRY-KISS-YAGNI-SOLID/Program.cs
@@ -1,3 +1,8 @@
+/* Переделка задания 1.2: 
+       https://github.com/N1stDev/LifeCycle-HomeWorks/tree/master/Task1_2
+   Условие:               
+       https://github.com/N1stDev/LifeCycle-HomeWorks/blob/master/Tasks/%D0%97%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5%202.pdf */
+
 using System;
 
 namespace Task1_2
